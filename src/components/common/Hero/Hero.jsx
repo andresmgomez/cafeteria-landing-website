54794d26a936d09ddec00233e4a5fc40071c96c1
Miguel Gomez <andresmgomez23@gmail.com>
2022-05-30 21:05:19 -0400

    Insert hero pictures and add slider effect using react-slider

--- src/components/common/Hero/Hero.jsx
+++ src/components/common/Hero/Hero.jsx
@@ -1,8 +1,29 @@
 import { Button, Container, Row, Col } from 'react-bootstrap';
+
+import Slider from 'react-slick';
 import classes from './Hero.module.css';
-import mainBackground from '../../../assets/img/hero-bg-1.jpeg';
+import styling from './HeroSlider.module.css';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import heroPicture1 from '../../../assets/img/hero-bg-1.jpeg';
+import heroPicture2 from '../../../assets/img/hero-bg-2.jpeg';
+import heroPicture3 from '../../../assets/img/hero-bg-3.jpeg';
+import heroPicture4 from '../../../assets/img/hero-bg-4.jpeg';
 
 export function HeroComponent() {
+	const settings = {
+		autoplay: true,
+		autoplaySpeed: 4000,
+		speed: 600,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		pauseOnHover: false,
+		dotsClass: true,
+		draggable: false,
+		arrows: false,
+	};
 	return (
 		<section className={`${classes.heroArea}`}>
 			<Container fluid>
@@ -20,14 +41,40 @@ export function HeroComponent() {
 					</Col>
 					<Col lg={8}>
 						<div className={`${classes.rightHero}`}>
-							<div className='item'>
-								<div className={`${classes.fillPicture}`}>
-									<img
-										src={mainBackground}
-										alt='Local people discovering our exclusive coffee place'
-									/>
+							<Slider className={`${styling.heroSlider}`} {...settings}>
+								<div className='item'>
+									<div className={`${classes.fillPicture}`}>
+										<img
+											src={heroPicture1}
+											alt='Local people discovering our exclusive coffee place.'
+										/>
+									</div>
 								</div>
-							</div>
+								<div className='item'>
+									<div className={`${classes.fillPicture}`}>
+										<img
+											src={heroPicture2}
+											alt='Barista serving our deluxe coffee in delicate cups.'
+										/>
+									</div>
+								</div>
+								<div className='item'>
+									<div className={`${classes.fillPicture}`}>
+										<img
+											src={heroPicture3}
+											alt='Glamorous lady enjoying our coconut cream machiatto.'
+										/>
+									</div>
+								</div>
+								<div className='item'>
+									<div className={`${classes.fillPicture}`}>
+										<img
+											src={heroPicture4}
+											alt='Another lady enjoying our coffee cream capuccino cup.'
+										/>
+									</div>
+								</div>
+							</Slider>
 						</div>
 					</Col>
 				</Row>
